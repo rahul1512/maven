f87abec37a7cf5f49a8e6a63b46be352ab8c2dc4
Deepak <Deepak@Deepak-PC>
2020-11-11 12:28:57 +0530

    initial commit

---
+++ src/main/java/maven/Test.java
@@ -0,0 +1,15 @@
+package maven;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(StringEscapeUtils.escapeJava("abc / xyz"));
+		System.out.println("Rahul");
+		System.out.println("Gupta");
+		System.out.println();
+	}
+
+}
